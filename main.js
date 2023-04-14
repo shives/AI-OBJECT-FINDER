f97574b8a14972a45a67e2d22440f66a04d12fd8
shives <87254612+shives@users.noreply.github.com>
2023-04-14 21:15:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+
+function setup() {
+canvas = createCanvas(480, 380);
+canvas.center();
+}
+
+function start() 
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("input").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
